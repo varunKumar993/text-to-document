5a531ae4dae89315a30a6cb646d1d92b212d1cda
varun kumar <92146756+varunKumar993@users.noreply.github.com>
2025-05-15 20:16:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const[text,setText] = useState("")
+  const[document,setDocumenet] = useState('')
+  const[loading,setLoading] = 
+  <>
+  <div className="body-container">
+    <form onSubmit={}>
+      <textarea onKeyDown={(e)=>{
+if(e.key=='Enter')
+{
+  handlesubmit(e)
+}
+     }} className="textarea"
+     value={text}></textarea></form></div></>;
+
+    }
+export default App;
