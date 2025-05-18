3f0449f0e594a082dedd8875d0b7eec0c5c5bf87
varun kumar <92146756+varunKumar993@users.noreply.github.com>
2025-05-18 08:38:09 +0530

    new jsx & CSS code added

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,45 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const[text,setText] = useState("")
-  const[document,setDocumenet] = useState('')
-  const[loading,setLoading] = 
-  <>
-  <div className="body-container">
-    <form onSubmit={}>
-      <textarea onKeyDown={(e)=>{
-if(e.key=='Enter')
-{
-  handlesubmit(e)
-}
-     }} className="textarea"
-     value={text}></textarea></form>
-     <textarea name="" id="" cols="30" rows="10"></textarea></div></>
-
-     ;
-
+  const [text, setText] = useState("");
+  const [document, setDocumenet] = useState("");
+  const [loading, setLoading] = useState(false)
+  const handlesubmit =(e)=>{
+    if(!loading){
+      setLoading(true)
+      e.preventDefault()
+      insertTextInDocument();
+      setText('')
     }
+  }
+    <>
+      <div className="body-container">
+        <form onSubmit={handlesubmit}>
+          <textarea
+            onKeyDown={(e) => {
+              if (e.key == "Enter") {
+                handlesubmit(e);
+              }
+            }}
+            className="textarea"
+            value={text}
+            placeholder="enter the text"
+            rows={7}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          ></textarea>
+        </form>
+        <div className="document-container">
+          <div>Document</div>
+          {document?(
+            <div className="document-body" dangerouslySetInnerHTML={{__html:document}}>
+              
+            </div>:(<div className="document-body">Added Content will show here</div>)
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
 export default App;
